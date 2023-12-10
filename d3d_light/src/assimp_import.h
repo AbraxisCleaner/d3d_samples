ed37749ecf10da02d0ab5157dcdd5206526fb50c
AbraxisCleaner <dalex5942@gmail.com>
2023-12-10 23:03:09 +1000

    Added assimp file.

---
+++ d3d_light/src/assimp_import.h
@@ -0,0 +1,10 @@
+#ifndef _ASSIMP_IMPORT_H_
+#define _ASSIMP_IMPORT_H_
+#include "stdafx.h"
+
+#include "mesh_common.h"
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
+#endif
